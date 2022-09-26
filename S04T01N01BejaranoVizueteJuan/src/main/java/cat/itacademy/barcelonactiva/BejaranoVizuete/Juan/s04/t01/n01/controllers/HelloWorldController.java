1fc00db3271883567b1ffa9494128abea3e5b8d7
jbejaranov <jbejaranov@gmail.com>
2022-09-26 19:33:11 +0200

    PathVariable

--- S04T01N01BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t01/n01/controllers/HelloWorldController.java
+++ S04T01N01BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t01/n01/controllers/HelloWorldController.java
@@ -1,6 +1,6 @@
 package cat.itacademy.barcelonactiva.BejaranoVizuete.Juan.s04.t01.n01.controllers;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +12,11 @@ public class HelloWorldController {
 
 		return "Hola, " + nom + ". Estàs executant un projecte Maven.";
 	}
-	
+
+	@GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+	String saluda2(@PathVariable (required = false) String nom) {
+		
+		return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+	}
+
 }
